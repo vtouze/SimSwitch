c1f1cf77fab9109a48751828c3a749d29b884e33
Virgile <touzevirgile@gmail.com>
2024-08-01 16:24:51 +0200

    Test MainMenu

---
+++ SimSwitchProject/Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using Unity.VisualScripting;
+using TMPro;
+
+public class MainMenu : MonoBehaviour
+{
+    #region Fields
+    [Header("Game Object")]
+    [SerializeField] private GameObject _mainMenu = null;
+    [SerializeField] private GameObject _settingsMenu = null;
+    [SerializeField] private GameObject _quitCheck = null;
+    [Header("Text")]
+    [SerializeField] private TMP_Text _settings = null;
+    private float _fontSize = 75f;
+    //[Header("Animation")]
+    //private float _fadeAnimationTime = 1.95f;
+    //[SerializeField] private GameObject _fadeOutCircle = null;
+    //private bool _hasFinishedQuitAnimation = false;
+
+    #endregion Fields
+
+    #region Methods
+    void Start()
+    {
+        _mainMenu.SetActive(true);
+        _quitCheck.SetActive(false);
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OpenSettings()
+    {
+        _mainMenu.SetActive(false);
+        _settingsMenu.SetActive(true);
+        _settings.fontSize = _fontSize;
+    }
+
+    #region Quit Methods
+    public void QuitChecking()
+    {
+        _quitCheck.SetActive(true);
+    }
+    private void Update()
+    {
+        /*if (_hasFinishedQuitAnimation == true && _fadeAnimationTime > 0)
+        {
+            _fadeOutCircle.SetActive(true);
+            _fadeAnimationTime -= Time.deltaTime;
+        }
+
+        if (_fadeAnimationTime <= 0 && _hasFinishedQuitAnimation == true)
+        {
+            Application.Quit();
+        }*/
+    }
+
+    public void QuitY()
+    {
+        //_hasFinishedQuitAnimation = true;
+        Application.Quit();
+    }
+
+    public void QuitN()
+    {
+        _quitCheck.SetActive(false);
+    }
+    #endregion Quit Methods
+    #endregion Methods
+}
