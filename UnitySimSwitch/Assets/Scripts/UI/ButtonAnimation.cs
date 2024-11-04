f02028002b97f1a93c05804f973006467c7bbe2c
Virgile <touzevirgile@gmail.com>
2024-11-04 14:55:33 +0100

    Encyclopedia Import

--- UnitySimSwitch/Assets/Scripts/UI/ButtonAnimation.cs
+++ UnitySimSwitch/Assets/Scripts/UI/ButtonAnimation.cs
@@ -1,23 +1,3 @@
-/*using UnityEngine;
-using UnityEngine.UI;
-
-public class ButtonAnimation : MonoBehaviour
-{ 
-    private Button _button;
-    [SerializeField] private Vector3 _sizeUp = new Vector3(1.2f, 1.2f, 1);
-
-    private void Awake()
-    {
-        _button = gameObject.GetComponent<Button>();
-        _button.onClick.AddListener(DoAnimation);
-    }
-
-    private void DoAnimation()
-    {
-        LeanTween.scale(gameObject, _sizeUp, .1f);
-        LeanTween.scale(gameObject, Vector3.one, .1f).setDelay(.1f);
-    }
-}*/
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
